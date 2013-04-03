805b7690650056301899adbed5c6a8531c25c0b5
ShineISP <info@shineisp.com>
2013-04-03 20:15:44 +0200

    - Payment transaction module created - Admin Menu Updated
    Change-Id: I22bdb2e57da7e254101d76ed22b6d047d19aacb4
    Signed-off-by: ShineISP <info@shineisp.com>

---
+++ application/modules/admin/controllers/PaymentsController.php
@@ -0,0 +1,279 @@
+<?php
+
+/**
+ * PaymentsController
+ * Manage the payment transaction
+ * @version 1.0
+ */
+
+class Admin_PaymentsController extends Zend_Controller_Action {
+	
+	protected $payments;
+	protected $datagrid;
+	protected $session;
+	protected $translator;
+	
+	/**
+	 * preDispatch
+	 * Starting of the module
+	 * (non-PHPdoc)
+	 * @see library/Zend/Controller/Zend_Controller_Action#preDispatch()
+	 */
+	
+	public function preDispatch() {
+		$this->session = new Zend_Session_Namespace ( 'Admin' );
+		$this->payments = new Payments ();
+		$this->translator = Zend_Registry::getInstance ()->Zend_Translate;
+		$this->datagrid = $this->_helper->ajaxgrid;
+		$this->datagrid->setModule ( "payments" )->setModel ( $this->payments );		
+	}
+	
+	/**
+	 * indexAction
+	 * Create the User object and get all the records.
+	 * @return unknown_type
+	 */
+	public function indexAction() {
+		$redirector = Zend_Controller_Action_HelperBroker::getStaticHelper ( 'redirector' );
+		$redirector->gotoUrl ( '/admin/payments/list' );
+	}
+	
+	/**
+	 * indexAction
+	 * Create the User object and get all the records.
+	 * @return datagrid
+	 */
+	public function listAction() {
+		$this->view->title = "Payments list";
+		$this->view->description = "Here you can see all the payment transactions.";
+		$this->view->buttons = array(array("url" => "/admin/payments/new/", "label" => $this->translator->translate('New'), "params" => array('css' => array('button', 'float_right'))));
+		$this->datagrid->setConfig ( Payments::grid() )->datagrid ();
+	}
+	
+	/**
+	 * Load Json Records
+	 *
+	 * @return string Json records
+	 */
+	public function loadrecordsAction() {
+		$this->_helper->ajaxgrid->setConfig ( Payments::grid() )->loadRecords ($this->getRequest ()->getParams());
+	}
+	
+	
+	/**
+	 * searchProcessAction
+	 * Search the record 
+	 * @return unknown_type
+	 */
+	public function searchprocessAction() {
+		$this->_helper->ajaxgrid->setConfig ( Payments::grid() )->search ();
+	}
+	
+	/*
+	 *  bulkAction
+	 *  Execute a custom function for each item selected in the list
+	 *  this method will be call from a jQuery script 
+	 *  @return string
+	 */
+	public function bulkAction() {
+		$this->_helper->ajaxgrid->massActions ();
+	}
+	
+	/**
+	 * recordsperpage
+	 * Set the number of the records per page
+	 * @return unknown_type
+	 */
+	public function recordsperpageAction() {
+		$this->_helper->ajaxgrid->setRowNum ();
+	}
+	
+	/**
+	 * newAction
+	 * Create the form module in order to create a record
+	 * @return unknown_type
+	 */
+	public function newAction() {
+		$this->view->form = $this->getForm ( "/admin/payments/process" );
+		$this->view->title = "Payment Transaction Details";
+		$this->view->description = "Here you can handle the Payment Transaction parameters";
+		$this->view->buttons = array(array("url" => "#", "label" => $this->translator->translate('Save'), "params" => array('css' => array('button', 'float_right'), 'id' => 'submit')),
+									 array("url" => "/admin/payments/list", "label" => $this->translator->translate('List'), "params" => array('css' => array('button', 'float_right'))));
+		$this->render ( 'applicantform' );
+	}
+	
+	/**
+	 * resetAction
+	 * Reset the filter previously set
+	 */
+	public function resetAction() {
+		$NS = new Zend_Session_Namespace ( 'Admin' );
+		unset ( $NS->search_payments );
+		$this->_helper->redirector ( 'index', 'payments' );
+	}
+	
+	
+	/**
+	 * confirmAction
+	 * Ask to the user a confirmation before to execute the task
+	 * @return null
+	 */
+	public function confirmAction() {
+		$id = $this->getRequest ()->getParam ( 'id' );
+		$controller = Zend_Controller_Front::getInstance ()->getRequest ()->getControllerName ();
+		try {
+			if (is_numeric ( $id )) {
+				$this->view->back = "/admin/$controller/edit/id/$id";
+				$this->view->goto = "/admin/$controller/delete/id/$id";
+				$this->view->title = $this->translator->translate ( 'Are you sure to delete the record selected?' );
+				$this->view->description = $this->translator->translate ( 'If you delete the Payment Transaction the record will no longer available' );
+				
+				$record = $this->payments->getbyId( $id );
+				$this->view->recordselected = $record->reference;
+			} else {
+				$this->_helper->redirector ( 'list', $controller, 'admin', array ('mex' => $this->translator->translate ( 'Unable to process request at this time.' ), 'status' => 'error' ) );
+			}
+		} catch ( Exception $e ) {
+			echo $e->getMessage ();
+		}
+	}
+	
+	/**
+	 * deleteAction
+	 * Delete a record previously selected by the category
+	 * @return unknown_type
+	 */
+	public function deleteAction() {
+		$id = $this->getRequest ()->getParam ( 'id' );
+		try {
+			$this->payments->getbyId ( $id )->delete ();
+		} catch ( Exception $e ) {
+			$this->_helper->redirector ( 'list', 'payments', 'admin', array ('mex' => $this->translator->translate ( 'Unable to process request at this time.' ) . ": " . $e->getMessage (), 'status' => 'error' ) );
+		}
+		return $this->_helper->redirector ( 'list', 'payments', 'admin' );
+	}
+	
+	/**
+	 * editAction
+	 * Get a record and populate the application form 
+	 * @return unknown_type
+	 */
+	public function editAction() {
+		$form = $this->getForm ( '/admin/payments/process' );
+		$id = $this->getRequest ()->getParam ( 'id' );
+		
+		// Create the buttons in the edit form
+		$this->view->buttons = array(
+				array("url" => "#", "label" => $this->translator->translate('Save'), "params" => array('css' => array('button', 'float_right'), 'id' => 'submit')),
+				array("url" => "/admin/payments/list", "label" => $this->translator->translate('List'), "params" => array('css' => array('button', 'float_right'), 'id' => 'submit')),
+				array("url" => "/admin/payments/new/", "label" => $this->translator->translate('New'), "params" => array('css' => array('button', 'float_right'))),
+		);
+		
+		if (! empty ( $id ) && is_numeric ( $id )) {
+			$rs = $this->payments->getAllInfo ( $id, null, true );
+			
+			if (! empty ( $rs [0] )) {
+				$rs[0]['paymentdate'] = Shineisp_Commons_Utilities::formatDateOut($rs[0]['paymentdate']);
+				$form->populate ( $rs [0] );
+			}
+			
+			$this->view->buttons[] = array("url" => "/admin/payments/confirm/id/$id", "label" => $this->translator->translate('Delete'), "params" => array('css' => array('button', 'float_right')));
+				
+		}
+		
+		$this->view->title = "Payment Transaction Details";
+        $this->view->description = "Here you can edit the Payment Transaction information paramenters.";
+		
+		$this->view->mex = $this->getRequest ()->getParam ( 'mex' );
+		$this->view->mexstatus = $this->getRequest ()->getParam ( 'status' );
+		
+		$this->view->form = $form;
+		$this->render ( 'applicantform' );
+	}
+	
+	/**
+	 * processAction
+	 * Update the record previously selected
+	 * @return unknown_type
+	 */
+	public function processAction() {
+		$i = 0;
+		$redirector = Zend_Controller_Action_HelperBroker::getStaticHelper ( 'redirector' );
+		$form = $this->getForm ( "/admin/payments/process" );
+		$request = $this->getRequest ();
+		
+		// Create the buttons in the edit form
+		$this->view->buttons = array(
+				array("url" => "#", "label" => $this->translator->translate('Save'), "params" => array('css' => array('button', 'float_right'), 'id' => 'submit')),
+				array("url" => "/admin/payments/list", "label" => $this->translator->translate('List'), "params" => array('css' => array('button', 'float_right'), 'id' => 'submit')),
+				array("url" => "/admin/payments/new/", "label" => $this->translator->translate('New'), "params" => array('css' => array('button', 'float_right'))),
+		);
+		
+		// Check if we have a POST request
+		if (! $request->isPost ()) {
+			return $this->_helper->redirector ( 'list', 'payments', 'admin' );
+		}
+		
+		if ($form->isValid ( $request->getPost () )) {
+			// Get the id 
+			$id = $this->getRequest ()->getParam ( 'payment_id' );
+			
+			// Set the new values
+			if (is_numeric ( $id )) {
+				$this->payments = Payments::getbyId ( $id );
+			}
+			
+			// Get the values posted
+			$params = $form->getValues ();
+			
+			try {
+				$id = Payments::saveData($params);
+				$this->_helper->redirector ( 'edit', 'payments', 'admin', array ('id' => $id, 'mex' => 'The task requested has been executed successfully.', 'status' => 'success' ) );
+			
+			} catch ( Exception $e ) {
+				$this->_helper->redirector ( 'edit', 'payments', 'admin', array ('id' => $id, 'mex' => $this->translator->translate ( 'Unable to process request at this time.' ) . ": " . $e->getMessage (), 'status' => 'error' ) );
+			}
+			
+			$redirector->gotoUrl ( "/admin/payments/edit/id/$id" );
+		} else {
+			$this->view->form = $form;
+			$this->view->title = "Payment Transaction Edit";
+			$this->view->description = "Edit the Payment Transaction information";
+			return $this->render ( 'applicantform' );
+		}
+	}
+	
+	/**
+	 * getForm
+	 * Get the customized application form 
+	 * @return unknown_type
+	 */
+	private function getForm($action) {
+		$form = new Admin_Form_PaymentsForm ( array ('action' => $action, 'method' => 'post' ) );
+		return $form;
+	}
+	
+	/**
+	 * set_status
+	 * Set the status of all items passed
+	 * @param $items
+	 * @return void
+	 */
+	private function set_status($items) {
+		$request = $this->getRequest ();
+		$status = $request->getParams ( 'params' );
+		$params = parse_str ( $status ['params'], $output );
+		$status = $output ['status'];
+		
+		if (is_array ( $items ) && is_numeric ( $status )) {
+			foreach ( $items as $categoryid ) {
+				if (is_numeric ( $categoryid )) {
+					$this->payments->set_status ( $categoryid, $status );
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+	
+}
