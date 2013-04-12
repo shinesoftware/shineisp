eb00116f8303c7d20579f7654b77b0850a987189
ShineISP <info@shineisp.com>
2013-04-12 17:56:23 +0200

    - add the date format parameter in the General administration configuration tab
    Change-Id: I63dae4b5bb6dad9a353b507b7d69820690af3eab
    Signed-off-by: ShineISP <info@shineisp.com>

---
+++ application/configs/data/sql/20130412175100-settings-parameters.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO `settings_parameters` (`name`, `var`, `type`, `module`, `enabled`, `description`, `group_id`, `config`) VALUES
+('Date Format', 'dateformat', 'text', 'admin', 1, 'Select the date format style. eg: dd/MM/YYYY', 1, NULL);
