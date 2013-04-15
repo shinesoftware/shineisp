5fe5be1f99df28eebf65f1b903cced1d4c8af101
GUEST.it s.r.l <alessandro.corbelli@guest.it>
2013-04-15 15:30:36 +0200

    Added servers groups capability

--- application/models/ServersGroups.php
+++ application/models/ServersGroups.php
@@ -13,4 +13,75 @@
 class ServersGroups extends BaseServersGroups
 {
 
+	/**
+	 * grid
+	 * create the configuration of the grid
+	 */	
+	public static function grid($rowNum = 10) {
+		
+		$translator = Zend_Registry::getInstance ()->Zend_Translate;
+		
+		$config ['datagrid'] ['columns'] [] = array ('label' => null, 'field' => 'group_id', 'alias' => 'group_id', 'type' => 'selectall' );
+		$config ['datagrid'] ['columns'] [] = array ('label' => $translator->translate ( 'ID' ), 'field' => 'group_id', 'alias' => 'group_id', 'sortable' => true, 'searchable' => true, 'type' => 'string' );
+		$config ['datagrid'] ['columns'] [] = array ('label' => $translator->translate ( 'Name' ), 'field' => 'name', 'alias' => 'name', 'sortable' => true, 'searchable' => true, 'type' => 'string' );
+		$config ['datagrid'] ['fields'] = "group_id, name as name";
+		
+		$config ['datagrid'] ['dqrecordset'] = Doctrine_Query::create ()->select ( $config ['datagrid'] ['fields'] )->from ( 'ServersGroups sg' );
+		
+		$config ['datagrid'] ['rownum'] = $rowNum;
+		
+		$config ['datagrid'] ['basepath'] = "/admin/serversgroups/";
+		$config ['datagrid'] ['index'] = "group_id";
+		$config ['datagrid'] ['rowlist'] = array ('10', '50', '100', '1000' );
+		
+		$config ['datagrid'] ['buttons'] ['edit'] ['label'] = $translator->translate ( 'Edit' );
+		$config ['datagrid'] ['buttons'] ['edit'] ['cssicon'] = "edit";
+		$config ['datagrid'] ['buttons'] ['edit'] ['action'] = "/admin/serversgroups/edit/id/%d";
+		
+		$config ['datagrid'] ['buttons'] ['delete'] ['label'] = $translator->translate ( 'Delete' );
+		$config ['datagrid'] ['buttons'] ['delete'] ['cssicon'] = "delete";
+		$config ['datagrid'] ['buttons'] ['delete'] ['action'] = "/admin/serversgroups/delete/id/%d";
+		return $config;
+	}
+
+    /**
+     * getAllInfo
+     * Get a record by ID
+     * @param $id
+     * @return ARRAY Record
+     */
+    public static function getAllInfo($id, $fields = "*", $language_id = 1) {
+        try {
+            $dq = Doctrine_Query::create ()
+					            ->from ( 'ServersGroups sg' )
+					            ->where ( 'sg.group_id = ?', $id );
+					            
+            if($fields != "*"){
+            	$dq->select($fields);
+            }
+            
+            $hp = $dq->execute ( array (), Doctrine_Core::HYDRATE_ARRAY );
+            
+            if (isset ( $hp [0] )) {
+                return $hp [0];
+            } else {
+                return array ();
+            }
+        
+        } catch ( Exception $e ) {
+            die ( $e->getMessage () );
+        }
+    }   
+
+
+	/*
+	 * find
+	 * Find an item by its id
+	 */
+	public static function find($id){
+		return Doctrine::getTable ( 'ServersGroups' )->find ( $id );
+	}
+
+
+
 }
