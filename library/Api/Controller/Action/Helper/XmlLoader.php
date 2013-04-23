804d6aa213c54bfb00eb655b2a3ef1af0b6ee9df
GUEST.it s.r.l <assistenza@guest.it>
2013-04-23 16:54:25 +0200

    Add authentication to api.

--- library/Api/Controller/Action/Helper/XmlLoader.php
+++ library/Api/Controller/Action/Helper/XmlLoader.php
@@ -1,5 +1,5 @@
 <?
-class XmlLoader extends Zend_Controller_Action_Helper_Abstract{
+class Api_Controller_Action_Helper_XmlLoader extends Zend_Controller_Action_Helper_Abstract{
     
     public static function createFromArray( $array ) {
         echo "<pre>";
