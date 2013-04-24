adb195c5b43d88f2401a5322e2062cf0dff37f2a
GUEST.it s.r.l <alessandro.corbelli@guest.it>
2013-04-24 12:03:18 +0200

    Fix another bug

--- application/modules/default/forms/MailsForm.php
+++ application/modules/default/forms/MailsForm.php
@@ -23,9 +23,9 @@ class Default_Form_MailsForm extends Zend_Form
         	'maxLength'	  => 20,
             'description' => 'Write here the password. Choose a secure password. [min 6 chars - max 20 chars]',
             'class'       => 'text-input medium-input password-strength',
-        	'validators' => array(array('regex', false, '/^[a-zA-Z0-9\-\_\.\%\!\$]{6,20}$/')),
-            ),
-        ));
+        	'validators' => array(array('regex', false, '/^[a-zA-Z0-9\-\_\.\%\!\$]{6,20}$/'))
+            )
+        );
         
         $this->addElement('checkbox', 'active', array(
             'decorators'  => array('Composite'),
