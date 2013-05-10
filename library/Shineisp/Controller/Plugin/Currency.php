64bd417362ee4d01c5a75356a5199ffe07528e12
ShineISP <info@shineisp.com>
2013-05-10 11:02:46 +0200

    improve the language selection Default YML fixed

---
+++ library/Shineisp/Controller/Plugin/Currency.php
@@ -0,0 +1,19 @@
+<?php
+
+class Shineisp_Controller_Plugin_Currency extends Zend_Controller_Plugin_Abstract {
+	
+	public function routeShutdown(Zend_Controller_Request_Abstract $request) {
+		$currency = new Zend_Currency('IT');
+
+		$registry = Zend_Registry::getInstance();
+		
+		// Check if the config file has been created
+		$isReady = Shineisp_Main::isReady();
+
+		if($isReady){
+			// TODO: To be completed
+		}
+
+		$registry->set('Zend_Currency', $currency);
+	}
+}
