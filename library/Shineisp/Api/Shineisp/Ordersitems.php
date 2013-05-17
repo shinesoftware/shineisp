def6841b7fcf702b8c162308882233930a40a57b
GUEST.it s.r.l <assistenza@guest.it>
2013-05-17 16:56:12 +0200

    Add ordersitems to api.

---
+++ library/Shineisp/Api/Shineisp/Ordersitems.php
@@ -0,0 +1,10 @@
+<?
+class Shineisp_Api_Shineisp_Ordersitems extends Shineisp_Api_Shineisp_Abstract_Action  {
+    
+    public function checkIfComplete( $uuid ) {
+        
+        
+    }
+    
+}
+    
