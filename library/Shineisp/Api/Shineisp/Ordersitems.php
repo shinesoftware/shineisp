2ca6565741f484c93cbfe1532c31347f7f481e0b
GUEST.it s.r.l <assistenza@guest.it>
2013-05-17 17:59:36 +0200

    Check if orderitems is complete.

--- library/Shineisp/Api/Shineisp/Ordersitems.php
+++ library/Shineisp/Api/Shineisp/Ordersitems.php
@@ -2,8 +2,9 @@
 class Shineisp_Api_Shineisp_Ordersitems extends Shineisp_Api_Shineisp_Abstract_Action  {
     
     public function checkIfComplete( $uuid ) {
+        $this->authenticate();
         
-        
+        return OrdersItems::checkIfCompletedByUUID($uuid);
     }
     
 }
