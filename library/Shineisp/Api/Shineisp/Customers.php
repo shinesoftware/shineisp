f7124858cfb8810222fcfc1397b492fef0230d65
GUEST.it s.r.l <assistenza@guest.it>
2013-05-23 18:37:53 +0200

    Add API for get profile customers and save update.

--- library/Shineisp/Api/Shineisp/Customers.php
+++ library/Shineisp/Api/Shineisp/Customers.php
@@ -49,7 +49,31 @@ class Shineisp_Api_Shineisp_Customers extends Shineisp_Api_Shineisp_Abstract_Act
     public function get( $uuid ) {
         $this->authenticate();
         
-          
+        $customer   = Customers::findWithUuid($uuid);
+        if( empty($customer) ) {
+            return false;
+        }
+        
+        $fields = ' c.company as company, c.firstname as firstname, c.lastname as lastname,c.sex,c.email, c.taxpayernumber, c.vat, l.legalform_id as legalformid, 
+                    a.address as adress, a.city as city,a.code as code,a.country_id as countryid ,a.region_id as regionid,a.area as area, ct.code as countrycode
+                    ,cn.*,cnts.*';
+        $customerid = $customer['customer_id'];
+        $customer   = Customers::getAllInfo( $customerid, $fields );
+        unset($customer['password']);
+        return $customer;
+
+    }
+    
+    public function save( $uuid, $params ) {
+        $this->authenticate();
+        
+        $customer   = Customers::findWithUuid($uuid);
+        if( empty($customer) ) {
+            return false;
+        }
+        
+        $customerid = $customer['customer_id'];
+        Customers::saveAll( $customerid, $params );
     }
     
 }
