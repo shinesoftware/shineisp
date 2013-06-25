f7860b0487df0a1a059b255db09200bafac320be
ShineISP <info@shineisp.com>
2013-06-25 09:02:27 +0200

    CSS foundation test integration alpha fixed minor bugs

--- application/modules/default/views/helpers/Webmenu.php
+++ application/modules/default/views/helpers/Webmenu.php
@@ -4,6 +4,7 @@
  */
 class Zend_View_Helper_Webmenu extends Zend_View_Helper_Abstract {
 	public $view;
+	protected $translator;
 	
 	public function setView(Zend_View_Interface $view) {
 		$this->view = $view;
@@ -14,28 +15,60 @@ class Zend_View_Helper_Webmenu extends Zend_View_Helper_Abstract {
 	 * Create the menu
 	 */
 	public function webmenu() {
+		$this->translator = Shineisp_Registry::get ( 'Zend_Translate' );
 		$NS = new Zend_Session_Namespace ( 'Default' );
 		if (!empty($NS->customer)) {
 			$this->view->user = $NS->customer;
 		}
-		$translation = Shineisp_Registry::getInstance ()->Zend_Translate;
-		$locale = $translation->getAdapter ()->getLocale ();
-		if(strlen($locale)==2){
-			$locale = $locale . "_" . strtoupper($locale);
+		
+		$menu = array('items' => array(), 'parents' => array());
+
+		// Get the store categories product
+		$categories = ProductsCategories::getMenu ();
+		foreach($categories as $menuItem)
+		{
+			$menu['items'][$menuItem['category_id']] = $menuItem;
+			$menu['parents'][$menuItem['parent']][]  = $menuItem['category_id'];
 		}
 		
-		$this->view->cmslinks = $this->createMenu(0, $locale);
+		// Create CMS pages menu
+		$cmsmenu = '<li><a href="/">'.$this->translator->translate('Blog').'</a>' . $this->createMenu(0) . "<li>";
+		
+		// Create the tlds list menu
+		$tldmenu = $this->createTldMenu();
+		
+		// Create the store categories menu
+		$storecategories = $this->buildMenu(0, $menu);
+
+		// Delete the last </ul> in the list
+		$storecategories = substr_replace($storecategories ,"",-6);
+		
+		// Replace the header of the menu list
+		$storecategories = substr_replace($storecategories, $tldmenu . $cmsmenu, 0, strlen("<ul class=''>\n"));
+		
+		// we will send the body of the bullet list to the partial template
+		$this->view->menu = $storecategories;
+
 		return $this->view->render ( 'partials/webmenu.phtml' );
 	}
 	
-	private function createMenu($parent, $locale) {
+	/** 
+	 * Create a CMS menu
+	 * @param unknown_type $parent
+	 * @param unknown_type $locale
+	 * @return string
+	 */
+	private function createMenu($parent) {
+		
+		$locale = $this->translator->getAdapter ()->getLocale ();
+		
 		$children = CmsPages::getParent ( $parent, $locale );
 		$items = array ();
 		
 		if (is_array ( $children )) {
 			foreach ( $children as $row ) {
 				$link = ! empty ( $row ['link'] ) ? $row ['link'] : "/cms/" . $row ['var'] . ".html";
-				$items [] = "<li class=\"item\"><a href=\"" . $link . "\">" . $row ['title'] . "</a>" . $this->createMenu ( $row ['page_id'], $locale ) . "</li>";
+				$items [] = "<li class=\"item\"><a href=\"" . $link . "\">" . $row ['title'] . "</a>" . $this->createMenu ( $row ['page_id'] ) . "</li>";
 			}
 		}
 		if (count ( $items )) {
@@ -43,5 +76,61 @@ class Zend_View_Helper_Webmenu extends Zend_View_Helper_Abstract {
 		} else {
 			return '';
 		}
+	}
+	
+
+	private function buildMenu($parentId, $menuData, $deep=0)
+	{
+		$html = '';
+	
+		if (isset($menuData['parents'][$parentId]))
+		{
+			$class = ($deep) ? "dropdown" : "";
+				
+			$html = "<ul class='$class'>\n";
+			foreach ($menuData['parents'][$parentId] as $itemId)
+			{
+				$html .= '<li><a class="'.$class.'" href="/categories/' . $menuData['items'][$itemId] ['uri'] . '.html">' . $menuData['items'][$itemId]['name'] . "</a>";
+	
+				$deep++;
+	
+				// find childitems recursively
+				$html .= $this->buildMenu($itemId, $menuData, $deep);
+	
+				$deep--;
+	
+				$html .= "</li>\n";
+			}
+				
+			$html .= "</ul>\n";
+		}
+	
+		return $html;
+	}
+	
+	
+	/**
+	 *
+	 * Create the tld list
+	 *
+	 */
+	private function createTldMenu() {
+		$ns = new Zend_Session_Namespace ();
+		$items = DomainsTlds::getHighlighted($ns->langid);
+		$currency = Shineisp_Registry::get ( 'Zend_Currency' );
+	
+		$html = "<li>";
+		$html .= '<a href="/domainschk/index/" class="dropdown">'.$this->translator->translate('Domains').'</a>';
+		$html .= "<ul class=\"dropdown\">";
+		foreach ( $items as $item ) {
+			if(!empty($item ['DomainsTldsData'][0]['name'])){
+				$item ['registration_price'] = $currency->toCurrency($item ['registration_price'], array('currency' => Settings::findbyParam('currency')));
+				$html .= '<li><a title="' . Shineisp_Commons_Utilities::truncate(strip_tags($item ['DomainsTldsData'][0]['description']), 150, "...", false, true) . '" href="/tlds/' . $item ['DomainsTldsData'][0]['name'] . '.html">.<b>' . strtoupper($item ['DomainsTldsData'][0]['name']) . "</b> - " . $item ['registration_price'] . " (" . $this->translator->translate('VAT not included') . ")</a></li>";
+			}
+		}
+		$html .= "</ul>";
+		$html .= "</li>";
+	
+		return $html;
 	}
 }
---
+++ public/skins/default/responsive/js/cpanels.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+	
+	$("#searchbox").focus(function() {
+		$(this).parent().addClass("curfocus");
+		$(this).addClass("largebox");
+	});
+
+	$("#searchbox").blur(function() {
+		$(this).parent().removeClass("curfocus");
+		$(this).removeClass("largebox");
+	});
+	
+	//Custom Action button inside the list of the records
+	$(".chkdomain").click( 
+		function () {
+			if($('.domainame').val()){
+				$.post('/common/checkdomain/', {name: $('.domainame').val(), tld: $('.tld').val()}, 
+					function(data){
+						if(data.available){
+							$('#result').removeClass('big-notavailable');
+							$('#result').addClass('big-available');
+							$('a.ordernow').attr('href', '/domains/buy/tld/' + data.tld + '/name/' + data.name + '/do/register');
+						}else{
+							$('#result').removeClass('big-available');
+							$('#result').addClass('big-notavailable');
+							$('a.ordernow').attr('href', '/domains/buy/tld/' + data.tld + '/name/' + data.name + '/do/transfer');
+						}
+						$('#result').show();
+						$('.price').html(data.price);
+						$('.domain').html(data.domain);
+						$('.message').html(data.mex);
+						
+					}, 'json');
+			}
+			return false;
+		}
+	);
+	
+	tinyMCE.baseURL='/resources/js/wysiwyg/tiny_mce'; // your path to tinyMCE
+    tinyMCE.init({
+        // General options
+    	mode : "specific_textareas",
+        editor_selector : "wysiwyg",
+        theme : "simple",
+        width : "900",
+        height : "400",
+        convert_urls : false
+    });
+    
+    // Stars Reviews
+    $('.stars').each(function() {
+	    var codeid = $(this).attr('alt');
+	    var hovertip = "hovertip_" + $(this).attr('alt');
+	    
+	    $(this).rating({
+	          required: true, 
+		  	  callback: function(value, link){
+				  $.post("/tickets/vote/", { stars: value, id: codeid } );
+			  },
+		  
+			  focus: function(value, link){
+				var tip = $('#' + hovertip);
+				tip[0].data = tip[0].data || tip.html();
+				tip.html(link.title || 'value: '+value);
+			  },
+			  
+			  blur: function(value, link){
+				var tip = $('#' + hovertip);
+				$('#' + hovertip).html(tip[0].data || '');
+			  }
+	    });
+	});
+}); 
