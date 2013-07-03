2fa55a703c66057339ec99b6ecb99aaaf04698c7
ShineISP <info@shineisp.com>
2013-07-03 18:51:02 +0200

    CSS foundation test integration beta

--- application/modules/default/forms/SearchForm.php
+++ application/modules/default/forms/SearchForm.php
@@ -7,10 +7,10 @@ class Default_Form_SearchForm extends Zend_Form
         
         $this->addElement('text', 'q', array(
             'filters'     => array('StringTrim'),
-            'title'       => 'Type here what you are looking for',
+            'placeholder'       => 'Type here what you are looking for',
             'id'       	  => 'searchbar',
             'class'       => 'searchbar',
-            'size'        => '7'
+            'size'        => '30'
         ));
 
     }
