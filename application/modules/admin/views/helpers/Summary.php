6fea1a875c765c68b462a44c71279329195b4888
shineisp <info@shineisp.com>
2013-08-05 17:27:59 +0200

    - administration dashboard statistic improved

--- application/modules/admin/views/helpers/Summary.php
+++ application/modules/admin/views/helpers/Summary.php
@@ -1,24 +1,18 @@
 <?php
-/**
- *
- * @author shine software
- * @version
- */
+
 /**
  * Summary helper
  *
  * @uses viewHelper Zend_View_Helper
  */
+
 class Admin_View_Helper_Summary extends Zend_View_Helper_Abstract{
-	/**
-	 *
-	 */
+	
 	public function summary($year = null) {
-		$this->view->income_yearly = Orders::incomeYearly($year);
-		$this->view->income_quarter = Orders::incomeQuarter($year);
-		$this->view->income_graph_monthly = Orders::incomeMonthly($year);
-		$this->view->income_text_monthly = Orders::incomeMonthlyText($year);
-				
+		$this->view->income_yearly = Orders::incomes($year);
+		$this->view->income_quarter = Orders::incomes($year, 'quarter');
+		$this->view->income_text_monthly = Orders::incomes($year, 'month');
+		$this->view->income_graph_monthly = Orders::incomeGraphMonthly($year);
 		return $this->view->render ( 'partials/summary.phtml' );
 	}
 
