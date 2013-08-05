b33ae42cc1f9c560f4a9e790bcede054fd61d092
shineisp <info@shineisp.com>
2013-08-05 18:53:10 +0200

    - administration dashboard statistic improved

--- application/modules/admin/views/helpers/Summary.php
+++ application/modules/admin/views/helpers/Summary.php
@@ -12,7 +12,7 @@ class Admin_View_Helper_Summary extends Zend_View_Helper_Abstract{
 		$this->view->income_yearly = Orders::incomes($year);
 		$this->view->income_quarter = Orders::incomes($year, 'quarter');
 		$this->view->income_text_monthly = Orders::incomes($year, 'month');
-		$this->view->income_graph_monthly = Orders::incomeGraphMonthly($year);
+		$this->view->income_graph_monthly = Orders::incomes($year, 'month');
 		return $this->view->render ( 'partials/summary.phtml' );
 	}
 
