92ebe8e83ce001696ae2fcdba14f3432705e85b2
Fabio Bottan <bottanfabio@hotmail.it>
2013-08-24 16:13:34 +0200

    Purchases module continuation

--- application/models/PurchaseCategories.php
+++ application/models/PurchaseCategories.php
@@ -38,16 +38,64 @@ class PurchaseCategories extends BasePurchaseCategories
     
     	$config ['datagrid'] ['columns'] [] = array ('label' => null, 'field' => 'c.category_id', 'alias' => 'category_id', 'type' => 'selectall' );
     	$config ['datagrid'] ['columns'] [] = array ('label' => $translator->translate ( 'Id' ), 'field' => 'c.category_id', 'alias' => 'category_id', 'sortable' => true, 'searchable' => true, 'type' => 'string' );
-    	$config ['datagrid'] ['columns'] [] = array ('label' => $translator->translate ( 'Name' ), 'field' => 'name', 'alias' => 'name', 'sortable' => true, 'searchable' => true, 'type' => 'string' );
+    	$config ['datagrid'] ['columns'] [] = array ('label' => $translator->translate ( 'Category' ), 'field' => 'c.category', 'alias' => 'category', 'sortable' => true, 'searchable' => true, 'type' => 'string' );
     
     	$config ['datagrid'] ['fields'] = "category_id, category";
     	$config ['datagrid'] ['rownum'] = $rowNum;
     
     	$config ['datagrid'] ['dqrecordset'] = Doctrine_Query::create ()->select ( $config ['datagrid'] ['fields'] )->from ( 'PurchaseCategories c' );
     
-    	$config ['datagrid'] ['basepath'] = "/admin/filecategories/";
+    	$config ['datagrid'] ['basepath'] = "/admin/purchasescategories/";
     	$config ['datagrid'] ['index'] = "category_id";
     
     	return $config;
+    }
+    
+    /**
+     * getAllInfo
+     * Get all data with the File category ID
+     * @param $id
+     * @return Doctrine Record / Array
+     */
+    public static function getAllInfo($id, $fields = "*", $retarray = false) {
+    
+    	try {
+    		$dq = Doctrine_Query::create ()->select ( $fields )
+    		->from ( 'PurchaseCategories c' )
+    		->where ( "category_id = $id" )
+    		->limit ( 1 );
+    
+    		$retarray = $retarray ? Doctrine_Core::HYDRATE_ARRAY : null;
+    		$item = $dq->execute ( array (), $retarray );
+    
+    		return $item;
+    	} catch (Exception $e) {
+    		die ( $e->getMessage () );
+    	}
+    }
+    
+    /**
+     * findbyName
+     * Get a filecategory record using its name.
+     * @param $name
+     * @return unknown_type
+     */
+    public static function findbyName($name) {
+    	return Doctrine::getTable ( 'PurchaseCategories' )->findOneBy ( 'name', $name )->toArray();
+    }
+    
+    /**
+     * find
+     * Get a record by ID
+     * @param $id
+     * @return Doctrine Record
+     */
+    public static function find($id, $fields = "*", $retarray = false) {
+    	$dq = Doctrine_Query::create ()->select ( $fields )->from ( 'PurchaseCategories c' )
+    	->where ( "category_id = $id" )->limit ( 1 );
+    
+    	$retarray = $retarray ? Doctrine_Core::HYDRATE_ARRAY : null;
+    	$records = $dq->execute ( array (), $retarray );
+    	return $records;
     }
 }
