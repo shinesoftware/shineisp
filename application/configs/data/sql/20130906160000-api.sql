f54046c15b33cbbf4daf51ccccc796519db30c83
Michelangelo Turillo <info@shineisp.com>
2013-09-06 16:10:29 +0200

    new resource updated

--- application/configs/data/sql/20130906160000-api.sql
+++ application/configs/data/sql/20130906160000-api.sql
@@ -1,12 +1,6 @@
-INSERT INTO `admin_resources` (`resource_id`, `name`, `hidden`, `is_container`, `is_allowed`, `admin`, `module`, `controller`, `parent_id`) VALUES (NULL, 'API WSDL', '1', '0', '0', '1', 'api', 'wsdl', '0');
-INSERT INTO `admin_permissions` (`permission_id`, `resource_id`, `role_id`, `permission`) VALUES (NULL, LAST_INSERT_ID(), '4', 'allow');
-
-INSERT INTO `admin_resources` (`resource_id`, `name`, `hidden`, `is_container`, `is_allowed`, `admin`, `module`, `controller`, `parent_id`) VALUES (NULL, 'API SOAP', '1', '0', '0', '1', 'api', 'soap', '0');
-INSERT INTO `admin_permissions` (`permission_id`, `resource_id`, `role_id`, `permission`) VALUES (NULL, LAST_INSERT_ID(), '4', 'allow');
-
 INSERT INTO `admin_resources` (`resource_id`, `name`, `hidden`, `is_container`, `is_allowed`, `admin`, `module`, `controller`, `parent_id`) VALUES (NULL, 'API Products', '1', '0', '0', '1', 'api', 'products', '0');
 INSERT INTO `admin_permissions` (`permission_id`, `resource_id`, `role_id`, `permission`) VALUES (NULL, LAST_INSERT_ID(), '4', 'allow');
 
-INSERT INTO `admin_resources` (`resource_id`, `name`, `hidden`, `is_container`, `is_allowed`, `admin`, `module`, `controller`, `parent_id`) VALUES (NULL, 'API Orders', '1', '0', '0', '1', 'api', 'orders', '0');
+INSERT INTO `admin_resources` (`resource_id`, `name`, `hidden`, `is_container`, `is_allowed`, `admin`, `module`, `controller`, `parent_i`) VALUES (NULL, 'API Orders', '1', '0', '0', '1', 'api', 'orders', '0');
 INSERT INTO `admin_permissions` (`permission_id`, `resource_id`, `role_id`, `permission`) VALUES (NULL, LAST_INSERT_ID(), '4', 'allow');
 
