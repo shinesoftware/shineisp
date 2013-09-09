52c4122f3f0a28ecd273c654ee9648f159b01204
Michelangelo Turillo <info@shineisp.com>
2013-09-09 10:42:06 +0200

    fixed minor bugs in schema and permissions

--- application/configs/data/sql/20130909094400-apiresources.sql
+++ application/configs/data/sql/20130909094400-apiresources.sql
@@ -1,4 +1,4 @@
-DELETE FROM `shineisp`.`admin_resources` WHERE `admin_resources`.`module` = 'api';
+DELETE FROM `admin_resources` WHERE `admin_resources`.`module` = 'api';
 
 INSERT INTO `admin_resources` (`resource_id`, `name`, `hidden`, `is_container`, `is_allowed`, `admin`, `module`, `controller`, `parent_id`) VALUES (NULL, 'API Request', '1', '0', '0', '1', 'api', 'request', '0');
 INSERT INTO `admin_permissions` (`permission_id`, `resource_id`, `role_id`, `permission`) VALUES (NULL, LAST_INSERT_ID(), '4', 'allow');
