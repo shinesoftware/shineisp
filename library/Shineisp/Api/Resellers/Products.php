3c673d2b145d71540deb117a05c8f1ca78f13935
ShineISP <info@shineisp.com>
2013-09-12 08:36:10 +0200

    fixed minor bugs

---
+++ library/Shineisp/Api/Resellers/Products.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Get the reseller products
+ * @author shine software
+ */
+class Shineisp_Api_Resellers_Products extends Shineisp_Api_Abstract_Action  {
+    
+    public function get( $productid ){
+        $this->authenticate();
+    	
+    	if( empty( $productid ) ) {
+            throw new Shineisp_Api_Exceptions( 400002, ":: 'productid' field" );
+            exit();
+        }
+        
+        $product    = new Products ();
+        $details    = $product->getAllInfo ( $productid );
+        
+        $details['ProductsUpgrades'] = ProductsUpgrades::getItemsbyProductID($productid);
+        return $details;
+    }
+    
+}
