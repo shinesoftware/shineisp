c85aaba17adc40bc9922458131eca269ac450fee
ShineISP <info@shineisp.com>
2013-09-18 20:05:46 +0200

    Email template importer updated minor bug fixed

---
+++ library/Shineisp/Api/Shineisp/Customers.php
@@ -0,0 +1,49 @@
+<?
+class Shineisp_Api_Shineisp_Customers extends Shineisp_Api_Shineisp_Abstract_Action  {
+    
+    public function insert( $params ) {
+        $this->authenticate();
+        
+        $form = new Api_Form_CustomerForm ( array ('action' => '#', 'method' => 'post' ) );
+        
+        if( array_key_exists('countrycode', $params) ) {
+            $country_id     = Countries::getIDbyCode($params['countrycode']);
+            if( $country_id == null ) {
+                throw new Shineisp_Api_Shineisp_Exceptions( 400005, ":: 'countrycode' not valid" );
+                exit();
+            }
+            
+            unset($params['coutrycode']);
+            $params['country_id']   = $country_id;
+        }
+        
+        if( array_key_exists('provincecode',$params) && $params['provincecode'] != "" ) {
+            $params['area'] = $params['provincecode'];
+            unset($params['provincecode']);
+        }
+        
+        if ($form->isValid ( $params ) ) {
+            if( $params['status'] == false ) {
+                $params['status'] = 'disabled';    
+            }
+            
+            $idcustomers    = Customers::Create($params);
+            
+            $customer       = Customers::find($idcustomers);
+            return $customer['uuid'];
+        } else {
+            $errors     = $form->getMessages();
+            $message    = "";
+            foreach( $errors as  $field => $errorsField ) {
+                $message .= "Field '{$field}'<br/>";
+                foreach( $errorsField as $error => $describe ) {
+                    $message .=" => {$error} ({$describe})";
+                }
+            }
+            
+            throw new Shineisp_Api_Shineisp_Exceptions( 400004, ":\n{$message}" );
+            exit();
+        }
+    }
+    
+}
