4a9e84d827c6d728e137dcf285bb55cf51fefd1b
Michelangelo Turillo <info@shineisp.com>
2014-01-23 15:12:09 +0100

    minor bug fixed

--- application/models/DomainsTasks.php
+++ application/models/DomainsTasks.php
@@ -245,8 +245,6 @@ class DomainsTasks extends BaseDomainsTasks {
 	 * add a domain task to be done by the cron job 
 	 */
 	static public function AddTask($domain, $action) {
-		echo $domain;
-		die;
 		$task = new DomainsTasks ( );
 		$task->startdate = date ( 'Y-m-d H:i:s' );
 		$task->action = $action;
