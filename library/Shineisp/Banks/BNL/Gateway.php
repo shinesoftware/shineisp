d087a80fadedbaaa3491d389a344774fa308aa4a
Michelangelo Turillo <mturillo@shinesoftware.com>
2015-06-11 00:41:28 +0200

    New BNL ePOSitivity module for ShineISP

--- library/Shineisp/Banks/BNL/Gateway.php
+++ library/Shineisp/Banks/BNL/Gateway.php
@@ -18,32 +18,32 @@
 */
 
 class Shineisp_Banks_BNL_Gateway extends Shineisp_Banks_Abstract implements Shineisp_Banks_Interface {
-	
-	public function __construct($orderid) {
-		parent::__construct ( $orderid );
-		parent::setModule ( __CLASS__ );
-	}
-	
-	/**
-	 * CreateForm
-	 * Create the bank form
-	 * @return string
-	 */
-	public function CreateForm() {
-		$order      = self::getOrder ();
-		$bank       = self::getModule ();
-		$translator = self::getTranslator ();
+
+    public function __construct($orderid) {
+        parent::__construct ( $orderid );
+        parent::setModule ( __CLASS__ );
+    }
+
+    /**
+     * CreateForm
+     * Create the bank form
+     * @return string
+     */
+    public function CreateForm() {
+        $order      = self::getOrder ();
+        $bank       = self::getModule ();
+        $translator = self::getTranslator ();
         $html       = null;
 
-		// Check the bank account field value
-		if (empty ( $bank ['account'] )) {
-			return null;
-		}
-		
-		if ($order) {
+        // Check the bank account field value
+        if (empty ( $bank ['account'] )) {
+            return null;
+        }
 
-			$url = $bank ['test_mode'] ? $bank ['url_test'] : $bank ['url_official'];
-			$item_name = $translator->translate ( "Order No." ) . " " . $order['order_number'];
+        if ($order) {
+
+            $url = $bank ['test_mode'] ? $bank ['url_test'] : $bank ['url_official'];
+            $item_name = $translator->translate ( "Order No." ) . " " . $order['order_number'];
             list($tid, $ksig) = explode(":", $bank ['account']);
 
             $init = new Shineisp_Banks_BNL_Igfs_CgInit();
@@ -71,7 +71,7 @@ class Shineisp_Banks_BNL_Gateway extends Shineisp_Banks_Abstract implements Shin
                 $session = new Zend_Session_Namespace ( 'Default' );
                 $session->paymentid = $paymentId;
 
-                Shineisp_Commons_Utilities::logs ( "The bank replies with a new paymentID: $paymentId", "bnl_igfs.log" );
+                Shineisp_Commons_Utilities::logs ( "-----> The bank replies with a new paymentID: $paymentId", "bnl_igfs.log" );
 
                 header("location:" . $init->redirectURL);
                 die;
@@ -87,32 +87,31 @@ class Shineisp_Banks_BNL_Gateway extends Shineisp_Banks_Abstract implements Shin
                 die($html);
             }
 
-			return array('name' => $bank ['name'], 'description' => $bank ['description'], 'html' => $html);
-		}
+            return array('name' => $bank ['name'], 'description' => $bank ['description'], 'html' => $html);
+        }
 
         return $translator->translate('There was a problem during the %s payment form creation', "BNL");
-	
-	}
-	
-	/**
-	 * Response
-	 * Create the Order, Invoice and send an email to the customer
-	 * @param $response from the Gateway Server
-	 * @return order_id or false
-	 */
-	public function Response($response) {
 
-	}
+    }
 
-	
-	/**
-	 * CallBack
-	 * This function is called by the bank server in order to confirm the transaction previously executed
-	 * @param $response from the Gateway Server
-	 * @return boolean
-	 */
-	public function CallBack($response) {
-		Shineisp_Commons_Utilities::logs ( "Start callback", "bnl_igfs.log" );
+    /**
+     * Response
+     * Create the Order, Invoice and send an email to the customer
+     * @param $response from the Gateway Server
+     * @return order_id or false
+     */
+    public function Response($response) {
+
+    }
+
+
+    /**
+     * CallBack
+     * This function is called by the bank server in order to confirm the transaction previously executed
+     * @param $response from the Gateway Server
+     * @return boolean
+     */
+    public function CallBack($response) {
 
         $bank = self::getModule ();
         $bankid = $bank ['bank_id'];
@@ -120,7 +119,7 @@ class Shineisp_Banks_BNL_Gateway extends Shineisp_Banks_Abstract implements Shin
 
         list($tid, $ksig) = explode(":", $bank ['account']);
 
-        Shineisp_Commons_Utilities::logs ( "Start the response.", "bnl_igfs.log" );
+        Shineisp_Commons_Utilities::logs ( "-----> Callback starts!", "bnl_igfs.log" );
 
         $session = new Zend_Session_Namespace ( 'Default' );
 
@@ -151,9 +150,9 @@ class Shineisp_Banks_BNL_Gateway extends Shineisp_Banks_Abstract implements Shin
         #return self::getOrderID ();
         Zend_Debug::dump($IgfsCgVerify);
         die;
-		// Get the orderid back from the bank post variables
-		$orderid = trim ( $response ['custom'] );
+        // Get the orderid back from the bank post variables
+        $orderid = trim ( $response ['custom'] );
 
-		return true;
-	}
+        return true;
+    }
 }
