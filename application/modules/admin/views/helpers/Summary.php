37f80a517e842635883af39531c1981606c9baba
Shine Software <info@shinesoftware.com>
2017-01-16 09:47:24 +0100

    Dashboard graph improved

--- application/modules/admin/views/helpers/Summary.php
+++ application/modules/admin/views/helpers/Summary.php
@@ -7,14 +7,14 @@
  */
 
 class Admin_View_Helper_Summary extends Zend_View_Helper_Abstract{
-	
-	public function summary($year = null) {
-		
-		$this->view->income_yearly = Orders::incomes();
-		$this->view->income_quarter = Orders::incomes(range(2011, date('Y')), 'quarter');
-		$this->view->income_text_monthly = Orders::incomes(array(), 'month');
-		$this->view->income_graph_monthly = Orders::incomes(array(), 'month');
-		return $this->view->render ( 'partials/summary.phtml' );
-	}
+
+    public function summary($year = null) {
+
+        $this->view->income_yearly = Orders::incomes();
+        $this->view->income_quarter = Orders::incomes(array(), 'quarter');
+        $this->view->income_text_monthly = Orders::incomes(array(), 'month');
+        $this->view->income_graph_monthly = Orders::incomes(array(), 'month');
+        return $this->view->render ( 'partials/summary.phtml' );
+    }
 
 }
