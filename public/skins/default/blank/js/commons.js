88372e4ecc5e50664b5f686a66e62a358d6bc91b
Shine Software <info@shinesoftware.com>
2017-04-12 11:57:17 +0200

    picture in product page fixed

--- public/skins/default/blank/js/commons.js
+++ public/skins/default/blank/js/commons.js
@@ -84,8 +84,11 @@ $(document).ready(function(){
 		}
 	});
 
-    $("#pop").on("click", function() {
-        $('#imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
-        $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+    $('.thumbnail').click(function(){
+        $('.modal-body').empty();
+        var title = $(this).parent('a').attr("title");
+        $('.modal-title').html(title);
+        $($(this).parents('div').html()).appendTo('.modal-body');
+        $('#myModal').modal({show:true});
     });
 });
