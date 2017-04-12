a3ad3c89c87efec8535a1c42ed00148b4a9d13d6
Shine Software <info@shinesoftware.com>
2017-04-12 15:03:41 +0200

    picture in product page fixed

--- public/skins/default/blank/js/commons.js
+++ public/skins/default/blank/js/commons.js
@@ -84,11 +84,12 @@ $(document).ready(function(){
 		}
 	});
 
-    $('.thumbnail').click(function(){
-        $('.modal-body').empty();
-        var title = $(this).parent('a').attr("title");
-        $('.modal-title').html(title);
-        $($(this).parents('div').html()).appendTo('.modal-body');
-        $('#myModal').modal({show:true});
+    $(".thumbnail").click(function(){
+        $(".modal-body").empty();
+        var title = $(this).parent("a").attr("title");
+
+        $(".modal-title").html(title);
+        $($(this).parents("div").html()).appendTo(".modal-body");
+        $("#myModal").modal({show:true});
     });
 });
