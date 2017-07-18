5616e28443cb3652455a530e95ac74500a05aea1
Shine Software Developers <info@shinesoftware.com>
2017-07-18 18:26:19 +0200

    XML Ticket Exporting tool has been improved

--- application/models/Tickets.php
+++ application/models/Tickets.php
@@ -859,7 +859,12 @@ class Tickets extends BaseTickets {
             $customer->addChildCData('company', $item['Customers']['company']);
             $customer->addChildCData('email', $item['Customers']['email']);
 
-            $category = $ticket->addChild('category',$item['TicketsCategories']['category']);
+            if(!empty($item['TicketsCategories']['category'])){
+                $category = $item['TicketsCategories']['category'];
+            }else{
+                $category = "";
+            }
+            $category = $ticket->addChild('category', $category);
             $category->addAttribute('category_id', $item['category_id']);
 
             $status = $ticket->addChild('status',$item['Statuses']['status']);
